7c6362a10a61d82201edbf4ea13d2b94d957ec54
CongLinh1003 <01633007559mc@gmail.com>
2023-10-01 20:12:36 +0700

    first-screen

--- App.js
+++ App.js
@@ -1,22 +1,32 @@
 import * as React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-
+import Svg, { Circle } from 'react-native-svg';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LinearGradient
-        // Background Linear Gradient
-        colors={['rgba(0,0,0,0.8)', 'transparent']}
-        style={styles.background}
-      />
-      <LinearGradient
-        // Button Linear Gradient
-        colors={['#4c669f', '#3b5998', '#192f6a']}
-        style={styles.button}>
-        <Text style={styles.text}> in with Facebook</Text>
-      </LinearGradient>
-    </View>
+    // Background Linear Gradient
+    <LinearGradient colors={['transparent', '#00CCF9']} style={styles.backgroundColor}>
+      <View style={styles.container}>
+
+
+        <svg xmlns="http://www.w3.org/2000/svg" width="140" height="140" viewBox="0 0 140 140" fill="none">
+          <circle cx="70" cy="70" r="62.5" stroke="black" stroke-width="15" />
+        </svg>
+        <text style={styles.heading}>GLOW YOUR BUSSINES</text>
+        <text style={styles.text}>We will help you to grow your business using
+          online server</text>
+        <View style={styles.viewBtn}>
+
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text }>LOGIN</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.text }>SIGN UP</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
+    </LinearGradient>
   );
 }
 
@@ -26,5 +36,46 @@ const styles = StyleSheet.create({
     width: '100%',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 25,
+    padding: '1rem'
+  },
+  viewBtn: {
+    width: '100%',
+    'flex-direction': 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 25,
+  },
+  backgroundColor: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+  },
+  button: {
+    color:'#000',
+    marginTop: 30,
+    backgroundColor: '#E3C000',
+    borderRadius: 5,
+    height: 40,
+    width: 120,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  heading: {
+    color: '#000',
+    textAlign: 'center',
+    fontFamily: 'Roboto',
+    fontSize: 25,
+    fontStyle: 'normal',
+    fontWeight: '700'
+  },
+  text: {
+    color: '#000',
+    textAlign: 'center',
+    fontSize: 18,
   },
 });
